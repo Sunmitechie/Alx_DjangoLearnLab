2a37e456bf39590ecfdd25c9219b9c8ddf56a375
Sunmibare Idowu <sunmijewel@gmail.com>
2025-03-02 13:35:54 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
-from relationship_app.views import list_books, LibraryDetailView, register_view, login_view, logout_view  # Replace 'your_actual_app_name' with the actual app name
-=======
+from relationship_app.views import list_books, LibraryDetailView, register_view, login_view, logout_view  
 from relationship_app.views import list_books, LibraryDetailView  
->>>>>>> 0b95bdeb56b88e1182e4eb55cfc16a96532f1f41
 from .views import list_books
 
 urlpatterns = [
