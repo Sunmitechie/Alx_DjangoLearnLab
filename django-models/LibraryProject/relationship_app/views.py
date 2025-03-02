5b6137b7d2dadd068921f6f835d14368e3784c7d
Sunmitechie <sunmijewel@gmail.com>
2025-03-02 18:24:02 +0100

    updated django app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,6 +18,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+from django.contrib.auth.decorators import user_passes_test
+
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -44,4 +46,22 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('login')
-    return render(request, 'logout.html')    
+    return render(request, 'logout.html')   
+from .models import UserProfile
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
