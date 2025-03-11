c97aaaefbe13a7b9a813b56efb1a683f26e4f031
Sunmitechie <sunmijewel@gmail.com>
2025-03-11 12:06:55 +0100

    added api project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
