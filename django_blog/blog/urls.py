b0da332d4b64b683c20d2edbf583cb2a73ce5429
Sunmitechie <sunmijewel@gmail.com>
2025-03-28 14:28:56 +0100

    Implemented CRUD Operations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path("post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/")
 ]
