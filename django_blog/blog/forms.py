67fddbe314a814bb7e07c4ac85b63fe207b18266
Sunmitechie <sunmijewel@gmail.com>
2025-03-29 11:13:16 +0100

    added tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -23,4 +23,4 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']        
+        fields = ['title', 'content', 'tags', "TagWidget()", "widgets"]        
